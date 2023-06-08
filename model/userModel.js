9df250f50734a58153df631209014015225229a3
Irechukwu Confidence <irechukwuconfidence@mail.com>
2023-06-08 13:51:08 +0100

    user model updated

--- model/userModel.js
+++ model/userModel.js
@@ -22,10 +22,26 @@ const userSchema = new mongoose.Schema({
     password:{
         type:String,
         required:[true,"password is required"],
-    },isBlocked:{
-        type:Boolean
-    }
-    
+    },
+    isBlocked:{
+        type:Boolean,
+        default:false,
+    },
+    isAdmin:{
+        type:Boolean,
+        default:false
+    },
+    role:{
+        type:String,
+        enum:["Admin","Guest"],
+        default:"Guest"
+    },
+    medical:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Medic"
+        }
+    ]
 },
 {
     timestamps:true,
