ed99ebdb1c691cab12ca32f883383d9d63cd8b46
Irechukwu Confidence <irechukwuconfidence@mail.com>
2023-06-09 12:53:52 +0100

    user create account created in user controller

--- controller/userController.js
+++ controller/userController.js
@@ -1,11 +1,29 @@
+import User from "../model/userModel.js"
 import AppError from "../utils/AppErr.js"
 
-export const createUserCtr = async(req,res) => {
+export const createUserCtr = async(req,res,next) => {
+    const { firstname, lastname, phoneNumber, email, password } = req.body;
     try {
+        //check if user has been registered before
+        const foundUser = await User.findOne({email});
+
+        if(foundUser){
+            return next(AppError(`user with this email:${email} already exist`,409))
+        }else{
+            const user = await User.create({
+                firstname,
+                lastname,
+                phoneNumber,
+                email,
+                password
+            })
+
         res.json({
-            status:"success",
-            data:"account created successfully"
+            status:"SUCCESS",
+            data: user
         })
+    }
+
     } catch (error) {
         next(AppError(error.message))
     }
