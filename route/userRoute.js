7a9cda76520536bf756e436a3c9b01368fc12b81
Irechukwu Confidence <irechukwuconfidence@mail.com>
2023-06-11 14:14:34 +0100

    isLogin added in user route

--- route/userRoute.js
+++ route/userRoute.js
@@ -1,14 +1,17 @@
 import express from "express"
 import {
      createUserCtr, 
+     profileCtr, 
      userLoginCtr 
     } from "../controller/userController.js";
+import { isLogin } from "../middlewears/isLogin.js";
 
 const userRoute = express.Router();
 
 //create user
 userRoute.post("/create",createUserCtr);
 userRoute.post("/login", userLoginCtr);
+userRoute.get("/profile",isLogin, profileCtr);
 
 
 
