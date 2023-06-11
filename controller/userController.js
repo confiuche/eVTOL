998fcb82c58336ba1617956a912b43cc2feeb380
Irechukwu Confidence <irechukwuconfidence@mail.com>
2023-06-11 14:13:58 +0100

    logic for login verify token

--- controller/userController.js
+++ controller/userController.js
@@ -2,6 +2,8 @@ import User from "../model/userModel.js"
 import AppError from "../utils/AppErr.js"
 import bcrypt from 'bcrypt'
 import generateToken from '../utils/generateToken.js';
+import { obtainTokenFromHeader } from "../utils/obtainTokenFromHeader.js";
+import { isLogin } from "../middlewears/isLogin.js";
 
 export const createUserCtr = async(req,res,next) => {
     const { firstname, lastname, phoneNumber, profilephoto, email, password } = req.body;
@@ -59,6 +61,27 @@ export const userLoginCtr = async(req,res,next) => {
                 email:isUserFound.email,
                 token:generateToken(isUserFound._id)
             }
+        })
+    } catch (error) {
+        next(AppError(error.message))
+    }
+}
+
+
+export const profileCtr = async (req, res, next) =>{
+    //userid = req.params.id
+    //console.log(userid);
+    
+    try {
+        const token = obtainTokenFromHeader(req)
+        const foundUser = await User.findById(req.userAuth)
+        if(!foundUser){
+            return next(AppError("No user associated with that id",404))
+        }
+
+        res.json({
+            status:"SUCCESS",
+            data:foundUser
         })
     } catch (error) {
         next(AppError(error.message))
