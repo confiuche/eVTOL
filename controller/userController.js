70ed1562ebb2838fa185058e56af56b93d343dfc
Irechukwu Confidence <irechukwuconfidence@mail.com>
2023-06-11 11:10:36 +0100

    hash password logic added

--- controller/userController.js
+++ controller/userController.js
@@ -1,8 +1,9 @@
 import User from "../model/userModel.js"
 import AppError from "../utils/AppErr.js"
+import bcrypt from 'bcrypt'
 
 export const createUserCtr = async(req,res,next) => {
-    const { firstname, lastname, phoneNumber, email, password } = req.body;
+    const { firstname, lastname, phoneNumber, profilephoto, email, password } = req.body;
     try {
         //check if user has been registered before
         const foundUser = await User.findOne({email});
@@ -10,12 +11,15 @@ export const createUserCtr = async(req,res,next) => {
         if(foundUser){
             return next(AppError(`user with this email:${email} already exist`,409))
         }else{
+            //hash password
+            const salt = await bcrypt.genSalt(10);
+            const hashPassword = await bcrypt.hash(password,salt);
             const user = await User.create({
                 firstname,
                 lastname,
                 phoneNumber,
                 email,
-                password
+                password:hashPassword,
             })
 
         res.json({
@@ -24,6 +28,20 @@ export const createUserCtr = async(req,res,next) => {
         })
     }
 
+    } catch (error) {
+        next(AppError(error.message))
+    }
+}
+
+
+//Login user
+export const userLoginCtr = async(req,res,next) => {
+    const {email, password} = req.body;
+    try {
+        res.json({
+            status:"SUCCESS",
+            data:"Login successfully"
+        })
     } catch (error) {
         next(AppError(error.message))
     }
