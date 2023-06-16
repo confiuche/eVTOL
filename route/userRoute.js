c36416449cbadd5bd54107f48b066cb7ea06c130
Irechukwu Confidence <irechukwuconfidence@mail.com>
2023-06-16 10:46:09 +0100

    route added for cloudinary

--- route/userRoute.js
+++ route/userRoute.js
@@ -5,6 +5,7 @@ import {
      forgetPasswordCtr, 
      passwordSettingCtr, 
      profileCtr, 
+     profilePhotoUploadCtrl, 
      resetPasswordCtr, 
      userLoginCtr 
     } from "../controller/userController.js";
@@ -12,13 +13,20 @@ import { isLogin } from "../middlewears/isLogin.js";
 import { isAdmin } from "../middlewears/isAdmin.js";
 import { isDispatch } from "../middlewears/isDispatch.js";
 import { validateUser } from "../middlewears/userValidation.js";
+import multer from 'multer';
+import storage from '../config/cloudinary.js'
 
 
 const userRoute = express.Router();
+const upload = multer({storage})
 
 //create user
-userRoute.post("/create",createUserCtr);
+userRoute.post("/create",validateUser,createUserCtr);
+//user login
 userRoute.post("/login", userLoginCtr);
+//upload profile
+userRoute.post("/profile-image",isLogin,upload.single("profile"),profilePhotoUploadCtrl)
+//user profile
 userRoute.get("/profile",isLogin, profileCtr);
 //forget password
 userRoute.post("/forget-password", forgetPasswordCtr)
