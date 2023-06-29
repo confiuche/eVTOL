c5bffca934420461870bf4df51d66dc356a3056c
Irechukwu Confidence <irechukwuconfidence@mail.com>
2023-06-29 15:06:37 +0100

    deleted

--- route/evtolRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { isLogin } from '../middlewears/isLogin.js'
-import { searchForEvtolAndPrice } from '../controller/evtolController.js'
-
-const evtolRoute = express.Router()
-
-evtolRoute.post("/search-for-eVTOL-model-and-price",isLogin,searchForEvtolAndPrice)
-
-export default evtolRoute
