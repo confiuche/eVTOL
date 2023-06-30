5e2509125df089ea62305fbe1f542531f000b948
Irechukwu Confidence <irechukwuconfidence@mail.com>
2023-06-30 07:35:53 +0100

    updated

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ import express from "express"
 import dotenv from "dotenv"
 import userRoute from "./route/userRoute.js";
 import bookingRoute from "./route/bookingRoute.js";
-import evtolRoute from "./route/evtolRoute.js";
 import locationRoute from "./route/locationRoute.js"
 import globalErrorHandler from "./middlewears/globalErrorHandler.js";
 import { database } from "./config/dBconnect.js";
+import dispatchRoute from "./route/dispatchRoute.js";
 
 
 
@@ -19,8 +19,8 @@ app.use(globalErrorHandler)
 const PORT = process.env.PORT || 8080
 
 app.use("/api/v1/evtol/users", userRoute)
-app.use("/api/v1/evtol/booking", bookingRoute)
+app.use("/api/v1/evtol", bookingRoute)
 app.use("/api/v1/evtol/location", locationRoute)
-app.use("/api/v1/evtol/search", evtolRoute)
+app.use("/api/v1/evtol",dispatchRoute)
 
 app.listen(PORT, console.log(`App Started at ${PORT}`))
