520920fbf49bbee40685c6b257c1453ecdef3328
Irechukwu Confidence <irechukwuconfidence@mail.com>
2023-07-02 13:51:31 +0100

    route for dispatch for loading, loaded, delivering, delivered and returning

--- route/dispatchRoute.js
+++ route/dispatchRoute.js
@@ -3,12 +3,23 @@ import { isLogin } from "../middlewears/isLogin.js"
 //import { isAdmin } from "../middlewears/isAdmin.js";
 import { isDispatch } from "../middlewears/isDispatch.js";
 import { 
-    loadedCtr 
+    deliveredCtr,
+    deliveringCtr,
+    displayAllBookedCtr,
+    loadedCtr, 
+    loadingCtr, 
+    returningCtr
 } from "../controller/dispatchCtr.js";
 
 
 const dispatchRoute = express.Router();
 
-dispatchRoute.post("/dispatch/:id",isLogin,isDispatch,loadedCtr)
+dispatchRoute.get("/all-booked",isLogin, isDispatch, displayAllBookedCtr)
+dispatchRoute.post("/Loading/:trackingCode",isLogin,isDispatch,loadingCtr)
+dispatchRoute.post("/Loaded/:trackingCode",isLogin,isDispatch,loadedCtr)
+dispatchRoute.post("/Delivering/:trackingCode",isLogin,isDispatch,deliveringCtr)
+dispatchRoute.post("/Delivered/:trackingCode",isLogin,isDispatch,deliveredCtr)
+dispatchRoute.post("/Returning/:trackingCode",isLogin,isDispatch,returningCtr)
+
 
 export default dispatchRoute
