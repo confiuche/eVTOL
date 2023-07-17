672c551ac301543b6089f147dffa3aa7913a2bcf
Irechukwu Confidence <irechukwuconfidence@mail.com>
2023-07-17 13:21:40 +0100

    startPayment createPayment and getPayment controller created

--- controller/userController.js
+++ controller/userController.js
@@ -6,6 +6,7 @@ import { obtainTokenFromHeader } from "../utils/obtainTokenFromHeader.js";
 import { isLogin } from "../middlewears/isLogin.js";
 import jwt from 'jsonwebtoken'
 import sendEmail from "../utils/emailUtils.js";
+import PaymentService from "../service/payment.service.js";
 
 export const createUserCtr = async(req,res,next) => {
     const { firstname, lastname, phoneNumber, profilephoto, email, password } = req.body;
@@ -272,3 +273,35 @@ export const forgetPasswordCtr = async(req, res, next) => {
     }
   }
 
+
+
+const paymentInstance = new PaymentService();
+
+export const startPayment = async(req, res) => {
+  try {
+    const response = await paymentInstance.startPayment(req.body);
+    res.status(201).json({status: "Success", data : response});
+  } catch (error) {
+    res.status(500).json({status: "Failed", message : error.message});
+  }
+}
+
+
+export const createPayment = async(req, res) => {
+  try {
+    const response = await paymentInstance.createPayment(req.query);
+    res.status(201).json({status: "Success", data : response});
+  } catch (error) {
+    res.status(500).json({status: "Failed", message : error.message});
+  }
+}
+
+
+export const getPayment = async(req, res) => {
+  try {
+    const response = await paymentInstance.paymentReciept(req.body);
+    res.status(201).json({status: "Success", data : response});
+  } catch (error) {
+    res.status(500).json({status: "Failed", message : error.message});
+  }
+}
