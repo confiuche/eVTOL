38ad3aefa942ae299416fc7b5fff1d612d3d279e
Irechukwu Confidence <irechukwuconfidence@mail.com>
2023-07-17 13:22:29 +0100

    updated

--- route/bookingRoute.js
+++ route/bookingRoute.js
@@ -1,10 +1,18 @@
 import express from "express"
 import { isLogin } from '../middlewears/isLogin.js'
 import { 
-    createBookingCtr, searchForEvtolAndPrice 
+    createBookingCtr, 
+    deliveredCtr, 
+    deliveringCtr, 
+    displayAllBookedCtr, 
+    loadedCtr, 
+    loadingCtr, 
+    returningCtr, 
+    searchForEvtolAndPrice 
 } from "../controller/bookingController.js";
 import multer from 'multer';
 import storage from '../config/cloudinary.js'
+import { isDispatch } from "../middlewears/isDispatch.js";
 
 const upload = multer({storage});
 
@@ -14,6 +22,18 @@ const bookingRoute = express.Router();
 bookingRoute.post("/search-for-eVTOL-model-and-price",isLogin,searchForEvtolAndPrice)
 //create user
 bookingRoute.post("/book/:id",isLogin,upload.single("medicImage"),createBookingCtr);
+//display all booking
+bookingRoute.get("/all-booked",isLogin, isDispatch, displayAllBookedCtr)
+//Loading state
+bookingRoute.post("/dispatch/Loading/:trackingCode",isLogin,isDispatch,loadingCtr)
+//Loaded state
+bookingRoute.post("/dispatch/Loaded/:trackingCode",isLogin,isDispatch,loadedCtr)
+//Delivering state
+bookingRoute.post("/dispatch/Delivering/:trackingCode",isLogin,isDispatch,deliveringCtr)
+//Delivered state
+bookingRoute.post("/dispatch/Delivered/:trackingCode",isLogin,isDispatch,deliveredCtr)
+//Returning state
+bookingRoute.post("/dispatch/Returning/:trackingCode",isLogin,isDispatch,returningCtr)
 
 
 
--- route/dispatchRoute.js
+++ route/dispatchRoute.js
@@ -2,24 +2,12 @@ import express from "express"
 import { isLogin } from "../middlewears/isLogin.js"
 //import { isAdmin } from "../middlewears/isAdmin.js";
 import { isDispatch } from "../middlewears/isDispatch.js";
-import { 
-    deliveredCtr,
-    deliveringCtr,
-    displayAllBookedCtr,
-    loadedCtr, 
-    loadingCtr, 
-    returningCtr
-} from "../controller/dispatchCtr.js";
 
 
 const dispatchRoute = express.Router();
 
-dispatchRoute.get("/all-booked",isLogin, isDispatch, displayAllBookedCtr)
-dispatchRoute.post("/Loading/:trackingCode",isLogin,isDispatch,loadingCtr)
-dispatchRoute.post("/Loaded/:trackingCode",isLogin,isDispatch,loadedCtr)
-dispatchRoute.post("/Delivering/:trackingCode",isLogin,isDispatch,deliveringCtr)
-dispatchRoute.post("/Delivered/:trackingCode",isLogin,isDispatch,deliveredCtr)
-dispatchRoute.post("/Returning/:trackingCode",isLogin,isDispatch,returningCtr)
+
+
 
 
 export default dispatchRoute
